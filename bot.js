93a6e38f74177d19004c7750f97e5e41bfc71ac1
Amr <44549704+WallBall22@users.noreply.github.com>
2018-12-07 09:09:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('message', message => {
-            if (message.content.startsWith("m!rules")) {
+            if (message.content.startsWith("r!rules")) {
      let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
 .addField('     اولا ' ,' ممنوع السب ')
 .addField('     ثانيا ' ,' لا تسوي سبام  ')
 .addField('     ثالثا ' ,' لا تزعج الاخرين ')
 .addField('    رابعا' ,' ممنوع الاعلانات ')
 .addField('    خامسا' ,' احترم الاخرين ')
-.addField('    سادسا' ,' لا تنشر في الشات او بل خاص ')//Mal_Team
+.addField('    سادسا' ,' لا تنشر في الشات او بل خاص ')
 .addField('    سابعا' ,' لا تنشر روابط! ')
 .addField('    ثامنا' ,' لا تسوي سبام ايموجي ')
 .addField('    تاسعا' ,' لا تطلب رتبه الاداره ! ')
-.setColor('#dada16')
-  message.channel.sendEmbed(embed);//Mal_Team
+.setColor('RANDOM')
+  message.channel.sendEmbed(embed);
     }
 });
 
